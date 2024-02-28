5588273d805131b22fd6e196f989bfc43b2c5984
Luis Gustavo <87923018+Gutyk@users.noreply.github.com>
2024-02-28 09:46:10 -0300

    Basico de Kotlin
    Apenas o basico

---
+++ src/main/kotlin/Fundamentos/PrimeiroPrograma.kt
@@ -0,0 +1,15 @@
+package Fundamentos
+
+fun main(){
+    print("Hello World");
+    //Comentario mt foda
+
+    /*XD
+    XD
+    XD
+    XD
+    XD
+    XD
+    XD
+    XD*/
+}
---
+++ src/main/kotlin/Fundamentos/Var.kt
@@ -0,0 +1,10 @@
+package Fundamentos
+
+fun main(){
+    var a: Int;
+    var b = 2
+
+    a = 10;
+
+    print(a + b);
+}
